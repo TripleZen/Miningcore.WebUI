2204655128e1153c45ecfa2d3504bcff89a62f6f
TripleZen <132538886+TripleZen@users.noreply.github.com>
2023-09-28 20:54:24 +1000

    Radiant

--- js/miningcore.js
+++ js/miningcore.js
@@ -23,14 +23,14 @@
 
 
 // read WebURL from current browser
-var WebURL         = window.location.protocol + "//" + window.location.hostname + "/";  // Website URL is:  https://domain.com/
+var WebURL         = "https://pool.triplezen.org/";  // Website URL is:  https://domain.com/
 // WebURL correction if not ends with /
 if (WebURL.substring(WebURL.length-1) != "/")
 {
 	WebURL = WebURL + "/";
 	console.log('Corrected WebURL, does not end with / -> New WebURL : ', WebURL);
 }
-var API            = WebURL + "api/";   						// API address is:  https://domain.com/api/
+var API            = "http://ltc.triplezen.org:4000/api";   						// API address is:  https://domain.com/api/
 // API correction if not ends with /
 if (API.substring(API.length-1) != "/")
 {
